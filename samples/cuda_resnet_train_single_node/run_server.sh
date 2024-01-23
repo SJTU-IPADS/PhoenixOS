71723ab9d8187810eef330e4cd91d5c5b18f8765
zobinHuang <zobin1999@gmail.com>
2024-01-23 12:15:24 +0800

    support kernel meta cache

--- samples/cuda_resnet_train_single_node/run_server.sh
+++ samples/cuda_resnet_train_single_node/run_server.sh
@@ -1,12 +1,12 @@
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd $script_dir
-LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build ../../remoting/cuda/cpu/cricket-rpc-server
+LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build ../../remoting/cuda/cpu/cricket-rpc-server -n resnet -k ./resnet.txt
 
 # run with gdb
-# LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build gdb ../../remoting/cuda/cpu/cricket-rpc-server
+# LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build gdb --args ../../remoting/cuda/cpu/cricket-rpc-server -n resnet -k ./resnet.txt
 
 # run sys, hasn't success yet
-# nsys profile -t cuda,nvtx -o ./output -e LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build --force-overwrite true ../../remoting/cuda/cpu/cricket-rpc-server
+# nsys profile -t cuda,nvtx -o ./output -e LD_LIBRARY_PATH=../../remoting/cuda/submodules/libtirpc/install/lib:../../build --force-overwrite true ../../remoting/cuda/cpu/cricket-rpc-server -n resnet -k ./resnet.txt
 
 # to download the nsys result
 # scp -r hzb@meepo4:/disk1/hzb/projects/pos/reorg/phoenixos/samples/cuda_resnet_train_single_node/output.nsys-rep ~/Desktop
