d77b6410c55b9b9fdf775cce6e63888de282ab92
zobinHuang <zobin1999@gmail.com>
2024-10-19 01:55:19 +0800

    minor for debug

--- samples/cuda_resnet_train_migration/svr.sh
+++ samples/cuda_resnet_train_migration/svr.sh
@@ -3,7 +3,7 @@
 # >>>>>>>>>> common variables <<<<<<<<<<
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-SUDO=sudo
+SUDO=
 
 # >>>>>>>>>> action configurations <<<<<<<<<<
 container_id=0
@@ -43,8 +43,9 @@ start_server() {
         $SUDO docker exec -it $container_name bash
     else
         cd $script_dir && cd .. && cd ..
-        $SUDO docker run --gpus all -dit -v $PWD:/root --privileged --network=pos_net \
-                        --ip $ip_addr --ipc=host --name $container_name zobinhuang/pos_svr_base:11.3
+        # $SUDO docker run --gpus all -dit -v $PWD:/root --privileged --network=pos_net \
+        #                 --ip $ip_addr --ipc=host --name $container_name zobinhuang/pos_svr_base:11.3
+        $SUDO docker run --gpus all -dit -v $PWD:/root --privileged --ipc=host --name $container_name zobinhuang/pos_svr_base:11.3
         $SUDO docker exec -it $container_name bash
     fi
 }
