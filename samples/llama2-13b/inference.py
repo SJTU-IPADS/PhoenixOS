1203e8dc567668ae36ae5b8ecd97d719b34aaab9
zobinHuang <zobin1999@gmail.com>
2024-11-05 18:44:55 +0800

    fix llama samples

--- samples/llama2-13b/inference.py
+++ samples/llama2-13b/inference.py
@@ -1,26 +1,27 @@
 model_id = 'meta-llama/Llama-2-13b-chat-hf'
 
+import time
 import transformers
-from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from huggingface_hub import login
 login(token = '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')
 
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map='auto')
-model.save_pretrained('../model/llama2-13b')
+# model = AutoModelForCausalLM.from_pretrained(model_id, device_map='auto', use_auth_token=True)
+# model.save_pretrained('./model')
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-tokenizer.save_pretrained('../tokenizer/llama2-13b')
+# tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=True)
+# tokenizer.save_pretrained('./tokenizer')
 
-exit(0)
+# exit(0)
 
-# model = AutoModelForCausalLM.from_pretrained('./model').to('cuda:0')
-# tokenizer = AutoTokenizer.from_pretrained('./tokenizer')
+model = AutoModelForCausalLM.from_pretrained('./model').to('cuda:0')
+tokenizer = AutoTokenizer.from_pretrained('./tokenizer')
 
 # pip install accelerate
 # pip install -i https://pypi.org/simple/ bitsandbytes
 
 #Llama 2 Inference
-def stream(user_prompt):
+def stream(user_prompt, batch_size=1):
     system_prompt = 'You are a helpful assistant that provides accurate and concise responses'
 
     B_INST, E_INST = "[INST]", "[/INST]"
@@ -28,11 +29,23 @@ def stream(user_prompt):
 
     prompt = f"{B_INST} {B_SYS}{system_prompt.strip()}{E_SYS}{user_prompt.strip()} {E_INST}\n\n"
 
-    inputs = tokenizer([prompt], return_tensors="pt").to("cuda:0")
+    inputs = tokenizer([prompt for _ in range(0, batch_size)], return_tensors="pt").to("cuda:0")
 
-    streamer = TextStreamer(tokenizer)
+    # streamer = TextStreamer(tokenizer)
 
+    s_time = time.time()
     # Despite returning the usual output, the streamer will also print the generated text to stdout.
-    _ = model.generate(**inputs, streamer=streamer, max_new_tokens=500)
+    reply = model.generate(**inputs, max_new_tokens=20)
+    e_time = time.time()
+
+    num_tokens = 0
+    for response in reply:
+        num_tokens += list(response.size())[0]
+
+    thpr = num_tokens/((e_time-s_time)/60)
+    # print(f"throughput: {thpr} tokens / min")
+    # return thpr
+
+    return e_time-s_time, thpr, num_tokens
 
 stream('Count to ten')
